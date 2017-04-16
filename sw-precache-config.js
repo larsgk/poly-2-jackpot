e6f5bec76d094c8d53d7f3cb40ae41d5faabe743
Lars Knudsen <larsgk@gmail.com>
2017-04-16 07:10:30 +0200

    Experimental iOS MP3 support added

---
+++ sw-precache-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/*',
+  ],
+  navigateFallback: 'index.html',
+  replacePrefix: '/poly-2-jackpot'
+};
